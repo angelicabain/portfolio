f3149f831d112f9e2e88486a49076921b551fbf9
Angelica Bain <88165134+angelicabain@users.noreply.github.com>
2024-10-01 23:03:24 -0400

    inital commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+    return <nav> 
+    <a href="/"> Porfolio </a>
+    <div> 
+        <ul> 
+            <li> <a href ="#about"> About </a> 
+            </li>
+        </ul>
+    </div>
+    </nav>;
+}
