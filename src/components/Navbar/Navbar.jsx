daca1e9d2a2a0271e416486cd18addee4e28e33f
Angelica Bain <88165134+angelicabain@users.noreply.github.com>
2024-10-02 00:37:12 -0400

    nav bar implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React , {useState} from "react";
+import styles from "./Navbar.module.css"
+import {getImageUrl} from "../../utils"
 
 export const Navbar = () => {
-    return <nav> 
-    <a href="/"> Porfolio </a>
-    <div> 
-        <ul> 
+    const[menuOpen, setMenuOpen] = useState(false);
+
+    return (
+    <nav className = {styles.navbar}> 
+    <a className={styles.title} href="/"> Porfolio </a>
+    <div className = {styles.menu}> 
+        <img className = {styles.menuBtn} src={
+            menuOpen ? getImageUrl("nav/menuIcon.png")
+            : getImageUrl("nav/closeIcon.png")
+        } 
+        alt = "menu-button" 
+
+        onClick ={() => setMenuOpen(!menuOpen)}
+
+        />
+        
+
+        <ul className = {`${styles.menuItems} ${!menuOpen && styles.menuOpen}`}
+            onClick= {() => setMenuOpen(true)}
+        > 
+            
             <li> <a href ="#about"> About </a> 
             </li>
+            <li> <a href ="#experience"> Experience </a> 
+            </li>
+            <li> <a href ="#projects"> Projects </a> 
+            </li>
+            <li> <a href ="#contact"> Contact </a> 
+            </li>
         </ul>
+      
     </div>
-    </nav>;
+    </nav>
+    );
 }
