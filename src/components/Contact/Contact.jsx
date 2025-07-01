d23ef055d1933a26ad70a3d744b1f578b5b22ded
Angelica Bain <88165134+angelicabain@users.noreply.github.com>
2025-06-30 21:17:40 -0700

    summer 2025 update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./Contact.module.css";
 
-export const Contact = () => 
-{
-    return  (<footer id = "contact" className = {styles.container}> 
-    <div className = {styles.text}> 
-    <h2> Contact </h2> 
-    <p> Feel free to reach out! </p>
-    </div>
-    <ul className = {styles.links}> 
-    <li className = {styles.link}> 
-        <img src = {getImageUrl("contact/gmail.png")} alt = "Gmail Icon" /> 
-        <a href = "mailto:angelicaabain@gmail.com" > angelicabain@gmail.com </a> 
-    </li> 
-    <li className = {styles.link}> 
-        <img src = {getImageUrl("contact/linkedin.png")} alt = "LinkedIn Icon" /> 
-        <a href = "https://www.linkedin.com/in/angelicabain/" > linkedin.com/in/angelicabain </a> 
-    </li> 
-    <li className = {styles.link}> 
-        <img src = {getImageUrl("contact/github.png")} alt = "Github Icon" /> 
-        <a href = "https://github.com/angelicabain" > github.com/angelicabain </a> 
-    </li> 
-    </ul> 
-    </footer> 
+export const Contact = () => {
+    return (<footer id="contact" className={styles.container}>
+        <div className={styles.text}>
+            <h2> Contact </h2>
+            <p> Feel free to reach out! </p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/gmail.png")} alt="Gmail Icon" />
+                <a href="mailto:dcf3mm@virginia.edu" > dcf3mm@virginia.edu </a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/linkedin.png")} alt="LinkedIn Icon" />
+                <a href="https://www.linkedin.com/in/angelicabain/" > linkedin.com/in/angelicabain </a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/github.png")} alt="Github Icon" />
+                <a href="https://github.com/angelicabain" > github.com/angelicabain </a>
+            </li>
+        </ul>
+    </footer>
     )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,11 +5,11 @@ import styles from "./Projects.module.css";
 
 export const Projects = () => {
     return (
-        <section className = {styles.container} id="projects">
-            <h2 className = {styles.title}>Projects</h2>
-            <div className = {styles.projects}>
+        <section className={styles.container} id="projects">
+            <h2 className={styles.title}>recent projects</h2>
+            <div className={styles.projects}>
                 {projects.map((project, id) => {
-                return <ProjectCard key={id} project = {project} />;
+                    return <ProjectCard key={id} project={project} />;
 
                 })}
             </div>
